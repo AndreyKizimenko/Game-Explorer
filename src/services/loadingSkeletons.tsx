55fd355e05cde05f19a4fb02af4d1c9f50e8c365
AndreyKizimenko <andreykizimenko@gmail.com>
2024-01-08 17:54:36 -0600

    typo

--- src/services/loadingSkeletons.tsx
+++ src/services/loadingSkeletons.tsx
@@ -2,9 +2,9 @@ import { Skeleton } from "@chakra-ui/react";
 
 function generateSkeleton(count: number, height: string, width?: string) {
   const skeleton = Array.from({ length: count }, (_, index) => (
-    <Skeleton key={index} h={height} w={width}/>
+    <Skeleton key={index} h={height} w={width} />
   ));
-  return skeleton
-};
+  return skeleton;
+}
 
 export default generateSkeleton;
